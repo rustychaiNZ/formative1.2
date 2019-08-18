60c58acd00fc36cb0a61f300c920e26805299bf8
Russell Johnson <rusljohn@gmail.com>
2019-08-18 23:59:43 +1200

    fixed stuff

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		$('nav').fadeToggle('active');
 	})
 
-	//Button that allows you to close the hamburger menu
+	//Buttons that allows you to close the hamburger menu
 	$('#close, li').click(function(){
 		$('nav').fadeOut(500);
 	})
@@ -14,10 +14,4 @@ $(document).ready(function(){
 	$('nav').mouseleave(function(){
 		$('nav').fadeOut(500);
 	})
-
-	// Closes the hamburger menu by selecting an item
-	// $('navItem').click(function(){
-	// 	$('nav').fadeOut(500);
-	// })
-
 });
