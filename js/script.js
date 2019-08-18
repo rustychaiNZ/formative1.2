2d8891622d56fc4e53aa3fa7c59cb07bba2f60e9
Russell Johnson <rusljohn@gmail.com>
2019-08-18 17:08:25 +1200

    finished home page, started nav

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log('my script is working!!!');
+
+$(document).ready(function(){
+
+	// Opens the hamburger menu
+	$('#menu').click(function(){
+		$('nav').fadeToggle('active');
+	})
+
+	//Button that allows you to close the hamburger menu
+	$('#close').click(function(){
+		$('nav').fadeOut(500);
+	})
+
+	// Closes the hamburger menu
+	$('nav').mouseleave(function(){
+		$('nav').fadeOut(500);
+	})
+
+	// Closes the hamburger menu by selecting an item
+	$('navItem').click(function(){
+		$('nav').fadeOut(500);
+	})
+
+});
