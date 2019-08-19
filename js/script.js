25594c554872809f71cd3f2a3853d40c9221a311
Russell grant Johnson <932400859@bun2-206-14.local>
2019-08-19 15:25:54 +1200

    worked on menus

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
 
+// Navigation----------
 	// Opens the hamburger menu
 	$('#menu').click(function(){
 		$('nav').fadeToggle('active');
 	})
-
 	//Buttons that allows you to close the hamburger menu
 	$('#close, li').click(function(){
 		$('nav').fadeOut(500);
 	})
-
 	// Closes the hamburger menu
 	$('nav').mouseleave(function(){
 		$('nav').fadeOut(500);
 	})
+
+// Menu-------------
+	// Opens menu
+	$('#breakfast').click(function(){
+		$('#breakfastMenu').slideDown('500, active');
+	})
+	// Closes Menu
+	$('#breakfast').click(function(){
+		$('#breakfastMenu').slideUp(500);
+	})
+
+
+
 });
